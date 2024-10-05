2f8a110a3ba58e8654b1148b75cceec6f43b8d29
AndriHordiichuk <166404529+AndriHordiichuk@users.noreply.github.com>
2024-10-05 17:14:33 +0300

    commit

---
+++ js/task-1.js
@@ -0,0 +1,25 @@
+function isEnoughCapacity(products, containerSize) {
+    let volume = Object.values(products)
+        .reduce((acc, size) => {
+            return acc + size
+        }, 0);
+        return volume <= containerSize;
+}
+
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
+  
